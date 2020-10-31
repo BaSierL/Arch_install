50c4a531f2908ece287081d03f303192ae52d78f
Auroot <41459490+BaSierL@users.noreply.github.com>
2020-10-31 20:20:42 +0800

    Update Install Script V3.5 Module

---
+++ Module/useradd.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Author: Auroot/BaSierl
+# QQ： 2763833502
+# Description： Arch Linux 安装脚本 
+# URL Blog： https://basierl.github.io
+# URL GitHub： https://github.com/BaSierL/arch_install.git
+# URL Gitee ： https://gitee.com/auroot/arch_install.git
+
+mkdir /Archin 2&> /dev/null
+null="/dev/null"
+S_LINE(){
+    sed -n -e '/# %wheel ALL=(ALL) NOPASSWD: ALL/=' /etc/sudoers
+}
+if [ ! -e /Archin/USERNAMES ]; then 
+    UserName=$(whiptail --title "ArchLinux - UserName" --inputbox "Enter UserName:" 10 60  3>&1 1>&2 2>&3)  # 输入用户名 
+    UserPassword=$(whiptail --title "ArchLinux - Password" --passwordbox "Enter User Password and choose Ok to continue." 10 60 3>&1 1>&2 2>&3) # 设置密码
+    exitstatus=$?
+    if [ $exitstatus = 0 ]; then
+        useradd -m -g users -G wheel -s /bin/bash ${UserName}  # 新建用户
+        echo ${UserName}:${UserPassword} | chpasswd &> /dev/null # 设置密码
+        echo "${UserName}" > /Archin/UserName  # 备份
+        #---------------------------------------------------------------------------#
+        # 更改sudo 配置
+        #-----------------------------
+        if (whiptail --title "Configure Sudoers. Yes/No" --yesno "Auto Configure Sudoers." 10 60); then
+            SUDOERS_LIST=$(S_LINE)
+            chmod 770 /etc/sudoers  # 设置权限
+            sed -i "${SUDOERS_LIST}i %wheel ALL=\(ALL\) NOPASSWD: ALL" /etc/sudoers
+            chmod 440 /etc/sudoers  # 设置权限
+            whiptail --title "Configure Sudoers." --msgbox "Configure Sudoers succeeded. [OK]" 10 60   # 设置sudo成功
+        else
+            whiptail --title "Configure Sudoers." --msgbox "Has been skipped. [X]" 10 60   # 跳过设置sudo
+        fi
+    fi
+    RootPassword_A=$(whiptail --title "ArchLinux - Root Password" --passwordbox "Enter Root Password and choose Ok to continue." 10 60 3>&1 1>&2 2>&3)  # 输入第一次root密码
+    RootPassword_B=$(whiptail --title "ArchLinux - Root Password" --passwordbox "Again Enter Root Password and choose Ok to continue." 10 60 3>&1 1>&2 2>&3)  # 输入第二次root密码
+    exitstatus=$?
+    if [ ${RootPassword_A} == ${RootPassword_B} ]; then
+        echo root:${RootPassword_B} | chpasswd &> $null   # 输入两次正确，将在这里设置Root密码
+        whiptail --title "Configure Root Password." --msgbox "Root Password setting complete. [OK]" 10 60   # 提示配置root密码成功
+        echo "1" > /Archin/USERNAMES
+    else    
+        whiptail --title "Configure Root Password." --msgbox "Two passwords are inconsistent. [X]" 10 60    # 输入两次错误，返回信息
+        exit 30;    # 输入两次错误，返回错误值
+    fi
+fi
